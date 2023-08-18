9ee2a66385efec4e75fe9cd00014ad30f4fb3b70
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-08-18 12:41:08 +0800

    Add files via upload

---
+++ Examples.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    float pi;
+    int length;
+    int width;
+    int area;
+    pi = 3.1416;
+    cout<<"enter your length"<<"\n";
+    cin>>length;
+    cout<<"enter your width"<<"\n";
+    cin>>width;
+        area = pi*(width*length);
+        cout<<area;
+    
+
+    return 0;
+}
