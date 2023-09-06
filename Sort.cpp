081740bda3ceeaa7c6fcfadc6141cfa10e12ecc3
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-09-06 13:18:22 +0800

    Add files via upload

---
+++ Sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+	int  i;
+	int num = 5;
+	int numbers[] = {1,2,3,4,5};
+	for (int i = num -1; i>=0; i--)
+	{
+ 	cout<<numbers[i] <<"\n";
+ }
+ return 0;
+}
+
+
