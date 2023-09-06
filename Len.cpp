1547a06cda4f5ce85bbc0b2c92bce258bf15ffe8
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-09-06 13:18:32 +0800

    Add files via upload

---
+++ Len.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+
+ int i, newnum;
+ int numbers[5] = {1,2,3,4,5};
+int getArrayLength = sizeof(numbers)/sizeof(int);
+cout << getArrayLength;
+ 
+ }
